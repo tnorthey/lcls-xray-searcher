50066f6b65e77e5a041885ac7373396bb24d5c0d
Thomas Northey <northeyt@psanagpu109.pcdsn>
2022-05-18 14:52:33 -0700

    q and radial averaging done

--- combine_xint_binning_runs.py
+++ combine_xint_binning_runs.py
@@ -5,15 +5,20 @@ from myfunctions import *
 # write more general...
 zero_array = np.zeros((20, 8, 512, 1024))
 data_total = zero_array
+bins_total = np.zeros(20)
 
 for run in runs:
  print('Run: %s' % run)
  h5file = '%sxint_binning_run%d.h5' % (scratch_dir, run)
  print('Loading %s...' % h5file)
+ x = load_h5data(h5file,'bins')
+ if x==False: print('Skipping run %s' % run); continue
+ bins_total += x
  x = load_h5data(h5file,'data')
  if x==False: print('Skipping run %s' % run); continue
  data_total += x
 
 hf = h5py.File('combined_runs_xint_binning.h5', 'w')
-hf.create_dataset('data_total', data=data_total)
+hf.create_dataset('data', data=data_total)
+hf.create_dataset('bins', data=bins_total)
 hf.close()
--- myfunctions.py
+++ myfunctions.py
@@ -1,4 +1,5 @@
 import h5py
+import numpy as np
 
 # Load the "key" array from a h5 file
 def load_h5data(fname,key):
@@ -66,3 +67,37 @@ def get_xint():
   xint = evt_xint_pull.TotalIntensity() #; print('xint: ' + str(xint))
   if (xint < lower_threshold) or (xint >= upper_threshold): return False
 
+# print basic stats function
+def stats(dset):
+  print('length:  %d' % len(dset))
+  print('mean:    %f' % np.mean(dset))
+  print('median:  %f' % np.median(dset))
+  print('minimum: %f' % np.min(dset))
+  print('maximum: %f' % np.max(dset))
+  print('st. dev: %f' % np.std(dset))
+
+def radial_avg(q,data,nbins):
+  # radial avg:
+  nq = nbins + 1
+  q_max = np.max(q)
+  q_rad = np.linspace(0,q_max,nq)
+  #print('q_rad length = %d' % len(q_rad))
+  #print(q_rad.shape)
+  print('Averaging over %d bins...' % nbins)
+  I_rad = np.zeros(nbins)
+  for i in range(0, nbins):
+    #print('Iteration %d' % i)
+    #print('creating bin in q-range: %f - %f' % (q_rad[i], q_rad[i+1]))
+    #print(q >= q_rad[i])
+    #print(q < q_rad[i+1])
+    condition = np.logical_and(q >= q_rad[i], q < q_rad[i+1])
+    #print(condition)
+    tmp = np.where(condition, 1, 0)  # 1s and 0s array of matching the condition
+    counts = np.sum(tmp)  # count hits that match the condition
+    #print(tmp.shape)
+    #print(high_data.shape)
+    #print(np.multiply(low_data,tmp))
+    if counts > 0:
+      I_rad[i] = np.sum(np.multiply(data,tmp), axis=None) / counts
+    else: continue
+  return q_rad[0:nbins],I_rad
